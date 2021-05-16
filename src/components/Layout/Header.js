33e07d08cb245af8d714e646f5c635c1f5e179b6
Stefan Korbakov <stefankorbakov@gmail.com>
2021-05-16 17:06:51 +0200

    Added base Header component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,19 @@
 import { Fragment } from 'react';
-import classes from './Header.module.css'
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
 const Header = (props) => {
-  return <Fragment>
-    <header></header>
-    <div></div>
-  </Fragment>;
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['mailn-image']}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
