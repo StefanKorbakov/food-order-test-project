589a62211c41459f97a0da8a8688eb0b38993cd7
Stefan Korbakov <stefankorbakov@gmail.com>
2021-05-18 23:47:54 +0200

    Added AvailableMeals styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,5 @@
+import classes from './AvailableMeals.module.css';
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
@@ -28,7 +30,7 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
 
-  return <section>
+  return <section className={classes.meals}>
     <ul>
       {mealsList}
     </ul>
