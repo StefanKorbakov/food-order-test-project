80e419cc1e452fd8700ea46a37cf9ef4de12dcf3
Stefan Korbakov <stefankorbakov@gmail.com>
2021-05-19 11:05:19 +0200

    Added Card component for UI purposes

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
@@ -28,13 +29,15 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
 
-  return <section className={classes.meals}>
-    <ul>
-      {mealsList}
-    </ul>
-  </section>
-}
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
 
 export default AvailableMeals;
