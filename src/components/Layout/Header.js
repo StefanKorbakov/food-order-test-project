e44fd9d44aa333aa9ce814f62dc9fcf53ba688c5
Stefan Korbakov <stefankorbakov@gmail.com>
2021-05-22 19:17:48 +0200

    Added Cart component with dummy values

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
         <h1>ReactMeals</h1>
         <HeaderCartButton />
       </header>
-      <div className={classes['mailn-image']}>
+      <div className={classes['main-image']}>
         <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
